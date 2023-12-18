d024cb141b415acb66466f5bca19ee39b51b61a3
support@infraplus.net <support@infraplus.net>
2023-12-18 20:06:08 +0100

    FIX get sso cred script

--- aws_sso_export.py
+++ aws_sso_export.py
@@ -12,7 +12,7 @@ Prerequisites:
 
 Usage:
 1. Run the script: `python3 /usr/local/bin/aws_sso_export.py` to display export commands of AWS_* var
-2. Run `eval $(/usr/local/bin/aws_sso_export.py) to export var in the curent environment
+2. Run `eval $(/usr/local/bin/aws_sso_export.py)` to export var in the current environment
 3. The script sets AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and AWS_SESSION_TOKEN for the current session.
 4. Run your AWS-dependent tools (like Terraform) in the same session.
 
@@ -24,30 +24,62 @@ Note:
 import json
 import os
 import subprocess
+from pathlib import Path
+
+def find_newest_file(paths):
+    latest_time = None
+    latest_file = None
+
+    for path in paths:
+        if path.exists() and path.is_dir():
+            for file in path.iterdir():
+                if file.is_file() and (latest_time is None or file.stat().st_mtime > latest_time):
+                    latest_time = file.stat().st_mtime
+                    latest_file = file
+
+    return latest_file
+
+def get_profile_name():
+    config_path = Path(os.path.expanduser('~/.aws/config'))
+    profile_name = None
+
+    if config_path.is_file():
+        with open(config_path, 'r') as file:
+            for line in file:
+                if line.startswith('[profile '):
+                    # Extracting profile name
+                    profile_name = line.split()[1].strip('[]')
+                    break
+    return profile_name
 
 def main():
-    # Path to the AWS CLI cache directory
-    cli_cache_path = os.path.expanduser('~/.aws/cli/cache')
+    # Paths to the AWS CLI and SSO cache directories
+    sso_cache_path = Path(os.path.expanduser('~/.aws/sso/cache'))
+    cli_cache_path = Path(os.path.expanduser('~/.aws/cli/cache'))
 
-    # Find the most recently modified file in the CLI cache directory
-    files = [os.path.join(cli_cache_path, f) for f in os.listdir(cli_cache_path)]
-    latest_file = max(files, key=os.path.getmtime)
+    # Find the most recently modified file in both cache directories
+    latest_file = find_newest_file([sso_cache_path, cli_cache_path])
+
+    if not latest_file:
+        print("No AWS SSO cache files found.")
+        return
+
+    # Check if the newest file is under sso cache and execute aws command
+    if str(latest_file).startswith(str(sso_cache_path)):
+        profile_name = get_profile_name()
+        if profile_name:
+            subprocess.run(["aws", "s3", "ls", f"--profile", profile_name], stdout=subprocess.PIPE)
+            # Recheck for the latest file in cli cache after the command
+            latest_file = find_newest_file([cli_cache_path])
 
     # Extract the credentials from the file
     with open(latest_file) as f:
         data = json.load(f)
-        credentials = data['Credentials']
-
-    # Set the environment variables (only local process env !!)
-    os.environ['AWS_ACCESS_KEY_ID'] = credentials['AccessKeyId']
-    os.environ['AWS_SECRET_ACCESS_KEY'] = credentials['SecretAccessKey']
-    os.environ['AWS_SESSION_TOKEN'] = credentials['SessionToken']
-
-    # Print the environment variables (optional for verification)
-    print(f"export AWS_ACCESS_KEY_ID={credentials['AccessKeyId']}")
-    print(f"export AWS_SECRET_ACCESS_KEY={credentials['SecretAccessKey']}")
-    print(f"export AWS_SESSION_TOKEN={credentials['SessionToken']}")
+        credentials = data.get('Credentials') or data
+        if 'AccessKeyId' in credentials:
+            print(f"export AWS_ACCESS_KEY_ID={credentials['AccessKeyId']}")
+            print(f"export AWS_SECRET_ACCESS_KEY={credentials['SecretAccessKey']}")
+            print(f"export AWS_SESSION_TOKEN={credentials['SessionToken']}")
 
 if __name__ == "__main__":
     main()
-
